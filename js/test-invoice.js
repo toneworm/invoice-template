9831cc54abf49e3071aa3eebd4eaf60ab860d223
Rich <toneworm@gmail.com>
2013-07-25 00:36:24 +0100

    Added css, scss, jquery, js

---
+++ js/test-invoice.js
@@ -0,0 +1,14 @@
+var invoiceDetails = {
+	'name' : '',
+	'company' : 'My<br />Company',
+	'email' : 'my@email.com',
+	'phone' : '+441234567890',
+	'date' : '14/07/2013',
+	'address1' : 'My Company<br />My house, In the middle<br />Of<br />My<br />Street<br />EF12 3GH',
+	'address2' : 'Company Ltd<br />Address1, Address2<br />Address3<br />Town<br />City<br />County<br />AB12 3CD',
+	'work-definition' : '10 days work (1/2-3/2 & 6/2-10/2)',
+	'work-charge' : '10 days at &pound;1 p/d',
+	'total-charge' : '&pound;10.00',
+	'comments' : 'Please pay to:<br /><br />Jeremy Jeremies<br />HSBC<br />Sort code: 12-34-56<br />Acc No: 12345678',
+	'invoice-no' : '0001'
+}
